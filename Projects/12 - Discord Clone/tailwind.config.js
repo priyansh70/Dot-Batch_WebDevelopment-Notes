057aa02e8b9d71fcb49a0469f6a28b139c708efb
priyansh70 <priyansh70890@gmail.com>
2023-02-05 12:48:52 +0530

    add Discord Clone Project

---
+++ Projects/12 - Discord Clone/tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors:{
+        lightBlue : "#404EED"
+      },
+      fontFamily: {
+        'ggsans': ['ggsans-normal'],
+        
+      },
+      spacing: {
+        '30' : '120px'
+      }
+    },
+  },
+  plugins: [],
+}
