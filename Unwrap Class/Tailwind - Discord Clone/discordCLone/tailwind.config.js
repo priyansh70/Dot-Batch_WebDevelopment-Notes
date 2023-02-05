71607a5b17007b52a01fccb9fe1e548ac41b63d8
priyansh70 <priyansh70890@gmail.com>
2023-02-05 12:56:36 +0530

    add unwrap class discord clone

---
+++ Unwrap Class/Tailwind - Discord Clone/discordCLone/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  fontFamily:{
+    Whitney:["Whitney"],
+    Ginto:["Ginto"],
+    ggSans:["ggSans"],
+  },
+}
