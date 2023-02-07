67fce8d3386b4fafd2baf5589c6acb5da0c5d8e9
priyansh70 <priyansh70890@gmail.com>
2023-02-07 23:42:31 +0530

    ADd Lecture 18 JS1

---
+++ Java Script/Lecture18 - JS BASIC1/script.js
@@ -0,0 +1,52 @@
+console.log("Version2");
+
+// VAriables
+var num1;
+var num1;
+
+let n1;
+// let n1; error
+
+// Dynamic Typing
+let a = 5;
+console.log(a);
+a = "Priyansh Patel";
+console.log(a);
+
+// Reference Type
+// #1 - Object
+const obj = {
+  Name: "Priyansh",
+  Age: "##",
+};
+
+// #2 - Array
+let arr = ["Priyansh", 00];
+
+// Operator
+// Arithmetic
+let number1 = 10;
+let number2 = 20;
+
+let ans = number1 + number2;
+console.log(ans);
+ans = number1 - number2;
+console.log(ans);
+ans = number1 * number2;
+console.log(ans);
+ans = number1 / number2;
+console.log(ans);
+ans = number1 ** number2;
+console.log(ans);
+// etc
+
+// Assignment Operator 
+// = += -= *= /= etc
+
+// Comparison Operator 
+// > < <= >= === != etc
+
+// Turnary Operator 
+// condition ? true : false;
+
+// ETC 
