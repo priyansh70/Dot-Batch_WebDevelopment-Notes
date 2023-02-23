802e6337ca6648e4fb2ae14c7490cb3cee84973f
priyansh70 <priyansh70890@gmail.com>
2023-02-23 23:20:36 +0530

    add js project class code + unwrap

---
+++ Unwrap Class/JS - Share Modal/script.js
@@ -0,0 +1,15 @@
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+
+//   Modal open function
+const openModal = () => {
+  console.log("Modal is Open");
+  modal.classList.add("active");
+  overlay.classList.add("overlayactive");
+};
+
+// Modal close function
+const closeModal = () => {
+  modal.classList.remove("active");
+  overlay.classList.remove("overlayactive");
+};
