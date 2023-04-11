09b298e51e1338379baf63a20ce2f820cc060ffa
priyansh70 <priyansh70890@gmail.com>
2023-04-11 12:32:20 +0530

    update random gifs

--- React/03 - React Advanced/random-meme/src/components/Random.jsx
+++ React/03 - React Advanced/random-meme/src/components/Random.jsx
@@ -27,16 +27,14 @@ const Random = () => {
   return (
     <div className="w-1/2 h-[450px] bg-green-500 mx-auto rounded-lg border border-black flex flex-col items-center gap-y-5 mt-[15px]">
       <h1 className="text-3xl uppercase underline font-bold">A Random Gif</h1>
-      {
-        loading ? (<Spinner />) : (<img src={gif} width="450" />)
-      }
+      {loading ? <Spinner /> : <img src={gif} width="450" />}
       <button
         onClick={() => fetchData()}
-      className="w-10/12 bg-white text-xl py-2 rounded-lg font-bold"
+        className="w-10/12 bg-white text-xl py-2 rounded-lg font-bold"
       >
-      Generate
-    </button>
-    </div >
+        Generate
+      </button>
+    </div>
   );
 };
 
--- React/03 - React Advanced/random-meme/src/hooks/useGif.js
+++ React/03 - React Advanced/random-meme/src/hooks/useGif.js
@@ -1,32 +1,31 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
+import React from 'react'
+import axios from 'axios';
+import { useEffect } from 'react';
+import { useState } from 'react';
+
 
 const API_KEY = process.env.REACT_APP_GIPHY_API_KEY;
 const url = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`;
 
 const useGif = (tag) => {
-  const [gif, setGif] = useState("");
+  const [gif, setGif] = useState('');
   const [loading, setLoading] = useState(false);
 
   async function fetchData(tag) {
     setLoading(true);
-
-    const { data } = await axios.get(tag  ? `${url}&tag=${tag}` : url);  
-    const imageSource = data?.data?.images?.downsized_large?.url;
+    
+    const {data} = await axios.get(tag ? `${url}&tag=${tag}`  : url);
+    const imageSource = data.data.images.downsized_large.url;
     setGif(imageSource);
-    console.log(imageSource);
-    // const res = await fetch(tag ? `${url}&tag=${tag}` : url)
-    // const output = await res.json();
-    // const imageSource = output.data.images.downsized.url;
-    // setGif(imageSource)
     setLoading(false);
   }
+  
+  
+  useEffect( () => {
+    fetchData('car');
+  },[] )
 
-  useEffect(() => {
-    fetchData("car");
-  }, []);
-
-  return (gif, loading, fetchData);
+  return {gif, loading, fetchData};
 };
 
 export default useGif;
