7a7abc23b8a1c069dc61a3351ad33028c8809493
priyansh70 <priyansh70890@gmail.com>
2023-04-13 21:51:51 +0530

    update blog context app

--- React/03 - React Advanced/blogs-context-starter/src/Components/Spinner.jsx
+++ React/03 - React Advanced/blogs-context-starter/src/Components/Spinner.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const Spinner = () => {
   return (
-    <div className='spinner'>Spinner</div>
+    <div className='flex flex-col items-center justify-center h-[70vh]'>
+      <div className='loader'></div>
+      <p>Loading...</p>
+    </div>    
   )
 }
 
